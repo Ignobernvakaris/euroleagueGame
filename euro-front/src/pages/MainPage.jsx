8eb3f79c25d91eb2904ecdbf32e8297906e754a7
Ignobernvakaris <mantas.rutkauskas.vu@gmail.com>
2024-10-10 19:21:29 +0300

    added tournament table

--- euro-front/src/pages/MainPage.jsx
+++ euro-front/src/pages/MainPage.jsx
@@ -4,7 +4,18 @@ import Header from "../components/Header";
 const MainPage = () => {
   return (
     <div>
-      <Header />
+      <div>
+        <Header />
+      </div>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="container text-center">
+          <h1>Namu darbai atlikti</h1>
+          <p>This container is centered both horizontally and vertically.</p>
+        </div>
+      </div>
     </div>
   );
 };
--- euro-front/src/pages/PredictionTable.jsx
+++ euro-front/src/pages/PredictionTable.jsx
@@ -4,16 +4,49 @@ import Header from "../components/Header";
 const PredictionTable = () => {
   return (
     <div>
-      <div>
-        <Header />
-      </div>
+      <Header />
       <div
         className="d-flex justify-content-center align-items-center"
         style={{ height: "100vh" }}
       >
         <div className="container text-center">
-          <h1>Centered Bootstrap Container</h1>
-          <p>This container is centered both horizontally and vertically.</p>
+          <h1>Euroleague spelionė</h1>
+          <table className="table table-bordered table-striped mt-4">
+            <thead>
+              <tr>
+                <th scope="col">Vieta</th>
+                <th scope="col">Dalyvis</th>
+                <th scope="col">Taškai</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Dalyvis 1</td>
+                <td>10</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Dalyvis 2</td>
+                <td>8</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Dalyvis 3</td>
+                <td>6</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>Dalyvis 4</td>
+                <td>4</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>Dalyvis 5</td>
+                <td>2</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
