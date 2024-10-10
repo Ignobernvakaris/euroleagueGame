2752be399dfcf2598493f124d628c26d6336613b
Ignobernvakaris <mantas.rutkauskas.vu@gmail.com>
2024-10-10 19:12:46 +0300

    centered navbar

--- euro-front/src/components/Header.jsx
+++ euro-front/src/components/Header.jsx
@@ -6,13 +6,13 @@ import logoPic from "../assets/logo.jpg";  // Importuojame paveikslėlį
 const Header = () => {
   return (
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <div className="container"> 
-      <Link className="navbar-brand" to="/">
-          <img src={logoPic} alt="Logo" style={{ width: "110px", radius: "50%" }} /> {/* Paveikslėlis */}
+      <div className="container-fluid"> 
+      <Link className="navbar-brand" to="/mainPage">
+          <img src={logoPic} alt="Logo" style={{ width: "110px", borderRadius: "50%" }} /> {/* Paveikslėlis */}
         </Link>
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link className="nav-link" to="/mainPage">Pagrindinis puslapis</Link>
+            <Link className="nav-link" to="/mainPage">Spėti rezultatą</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to="/predictionTable">Turnyrinė lentelė</Link>
